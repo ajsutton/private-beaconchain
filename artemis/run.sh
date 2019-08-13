d681f51bc9627fbf499369d921a31e88cfb8edd1
Adrian Sutton <adrian.sutton@consensys.net>
2019-08-14 07:37:10 +1000

    Run Artemis using docker.

--- artemis/run.sh
+++ artemis/run.sh
@@ -1,5 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -euo pipefail
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd )"
-
-$DIR/dist/artemis/bin/artemis --config ${DIR}/config.toml --logging=DEBUG
+mkdir -p ${DIR}/output
+docker rm /artemis > /dev/null 2>&1 || true
+docker run --mount "type=bind,source=${DIR},target=/artemis" -p 8008:8008 --network="beacontest" --name="artemis" pegasyseng/artemis:develop \
+  --config="/artemis/config.toml" \
+  --logging=DEBUG
