39167ab1044bad6a47755fa36b6c15c89ddad962
Adrian Sutton <adrian.sutton@consensys.net>
2019-11-19 20:55:31 +1000

    Set work dir so logs become available in the output directory.

--- artemis/run.sh
+++ artemis/run.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd )"
 mkdir -p ${DIR}/output
 docker rm /artemis > /dev/null 2>&1 || true
-docker run --mount "type=bind,source=${DIR},target=/artemis" -p 8008:8008 --network="beacontest" --name="artemis" pegasyseng/artemis:0.8.2-SNAPSHOT \
+docker run --workdir "/artemis/output" --mount "type=bind,source=${DIR},target=/artemis" -p 8008:8008 --network="beacontest" --name="artemis" pegasyseng/artemis:0.8.2-SNAPSHOT \
   --config="/artemis/config.toml" \
   --logging=INFO
